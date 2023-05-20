f990f1610c9bbffe5aaa855bc778e026a0423d38
Roopak Mallik <70304320+RoopakMallik@users.noreply.github.com>
2023-05-20 23:23:33 +0530

    Add files via upload

---
+++ cryptodata(May2023).py
@@ -0,0 +1,27 @@
+import csv
+import requests
+
+url='http://api.coincap.io/v2/assets'
+
+headers={
+    'Accept':'application/json',
+    'Content-Type':'application/json'
+}
+response=requests.request("GET",url,headers=headers,data={})
+jsonfile=response.json()
+
+actualdata=[]
+csvheading=['ID','Rank','Symbol','Name','Price(USD)','Max Supply']
+
+
+
+for x in jsonfile['data']:
+    listing=[x['id'],x['rank'],x['symbol'],x['name'],x['priceUsd'],x['maxSupply']]
+    actualdata.append(listing)
+
+with open('cryptodata(May2023).csv','w',encoding='UTF8',newline='')as f:
+    writer=csv.writer(f)
+    writer.writerow(csvheading)
+    writer.writerows(actualdata)
+
+print("Conversion Completed")
